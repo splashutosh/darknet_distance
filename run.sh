2ed3ac9191cb7b8630022babc9a892d384108904
splashutosh <ashutosh8896@gmail.com>
2017-09-13 09:45:59 -0700

    added run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 ./darknet detector demo -c $1 cfg/coco.data cfg/yolo.cfg yolo.weights -w 3840 -h 1080 -thresh 0.378
 #./darknet detector demo -c 1 cfg/coco.data cfg/yolo.cfg yolo.weights -w 1920 -h 1080 -thresh 0.378
+./darknet detector demo cfg/coco.data cfg/yolo.cfg yolo.weights car.mov
 
